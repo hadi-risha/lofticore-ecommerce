0693f82d2280a4068850376c7385e47b5e1b3499
Hadi <hadiyakhadija62@gmail.com>
2025-02-09 21:44:22 +0530

    backend setup completed

---
+++ backend/controllers/authController.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+  console.log("signup controller");
+  res.send("signup");
+};
+
+export const login = (req, res) => {
+  console.log("login controller");
+  res.send("login");
+};
